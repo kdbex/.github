64b9dee7c27f9082486cad4133e97cbe5f58a71b
cuber <t.vinet@yahoo.fr>
2024-09-10 22:35:34 +0200

    Working version for dev prod

--- packager.sh
+++ packager.sh
@@ -5,7 +5,6 @@ mkdir $build_dir
 cp node.exe nssm.exe installer.bat server.bat "${build_dir}"
 cd "../extension"
 quasar build -m bex > /dev/null
-cp -r "dist/bex/." ${build_dir}
 cd "../server"
 npm run build > /dev/null
 cd $build_dir
